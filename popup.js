09bdd8d44b43b9d4d92f6f3db6e6fdd9ee0a28a8
Fabien LEFEBVRE <contact@d1ceward.com>
2018-06-13 11:36:39 +0200

    Added files

---
+++ popup.js
@@ -0,0 +1,78 @@
+restore_options();
+
+// ==> Handler functions
+// Handle paragraph count changes
+document.getElementById('pCountId').onchange = () => {
+  createContent()
+  save_options()
+}
+
+// Handle sentence count changes
+document.getElementById('pLengthId').onchange = () => {
+  createContent()
+  save_options()
+}
+
+// ==> Text generation functions
+// Split text by punctuation
+function textArray(text) {
+  text = text.split(/[.?!]+ /)
+  text.pop()
+  return text
+}
+
+function appendContent(content) {
+  var text = ''
+  var loopCount = document.getElementById('pLengthId').value
+  var pNum = document.getElementById('pCountId').value
+
+  // Paragraph
+  for (var pCount = 0; pCount < pNum; pCount++) {
+    // Sentence
+    for (var sCount = 0; sCount < loopCount; sCount++) {
+      var rand = Math.floor((Math.random()*19))
+      var sentence = content[rand]
+
+      // Append space after punctuation if not last
+      if (sCount == loopCount - 1)
+        text += sentence + '.'
+      else
+        text += sentence + '. '
+    }
+    if (pCount != pNum - 1)
+      text += "\n\n"
+  }
+  document.getElementById('contGenId').innerHTML = text
+}
+
+// Get example lorem text by xhr
+function createContent() {
+  var req = new XMLHttpRequest()
+
+  req.open('GET', chrome.extension.getURL('loremIpsum.txt'), true)
+  req.onreadystatechange = () => {
+    if (req.readyState == 4 && req.responseText) {
+      appendContent(textArray(req.responseText))
+      return req.responseText
+    }
+  }
+  req.send(null)
+}
+
+// ==> Settings function
+function save_options() {
+  var defaults = { 'form_values': { pCountId: '1', paragraphLengthId: '10' } }
+  var settings = { 'form_values': {} }
+
+  for (var key in defaults.form_values)
+    settings['form_values'][key] = document.getElementById(key).value
+  chrome.storage.sync.set(settings)
+}
+
+function restore_options() {
+  chrome.storage.sync.get('form_values', (settings) => {
+    for (var key in settings.form_values)
+      document.getElementById(key).value = settings.form_values[key]
+    createContent()
+  })
+}
